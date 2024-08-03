9514752b5c773635d27057f924fb79146c518e6a
dozaka <Thomas.BK.Do@gmail.com>
2024-08-03 12:09:49 +0900

    update workflow

--- AspireYouTubeSummariser.ServiceDefaults/obj/Debug/net8.0/AspireYouTubeSummariser.ServiceDefaults.AssemblyInfo.cs
+++ AspireYouTubeSummariser.ServiceDefaults/obj/Debug/net8.0/AspireYouTubeSummariser.ServiceDefaults.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspireYouTubeSummariser.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f8f23132471e15969b0097cd58715967723ebc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d14ec43cfd0e78ea703ed64f7e78bf90b2b60c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspireYouTubeSummariser.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspireYouTubeSummariser.ServiceDefaults")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
